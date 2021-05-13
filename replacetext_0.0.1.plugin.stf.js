234fb1a365c3546f3511e7a6a9e8f08daf7498dd
Parsa Yazdani <3parsa3@gmail.com>
2021-05-13 19:51:37 +1000

    Create replacetext_0.0.1.plugin.stf.js

---
+++ replacetext_0.0.1.plugin.stf.js
@@ -0,0 +1,17 @@
+// Metadata
+const meta = {
+    id: 'xxxxxxxx',
+    name: 'My Plugin',
+    author: 'Parsa Yazdani (Quix)',
+    version: '0.0.1',
+    link: 'https://github.com/QuixThe2nd/starfiles-plugin-demo',
+    compatibility:{
+        website: true
+    }
+}
+// Declare Plugin
+window['start_' + meta.id] = function(){
+    document.body.innerHTML = document.body.innerHTML.replace("Starfiles", "SimonGay")
+}
+// Run Plugin
+init_plugin(meta);
